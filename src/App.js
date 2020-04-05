6f9bf3bd24e64cd15265729a3e2fbc6c7b4bae08
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-04-06 01:06:32 +0700

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-// import {  } from 'react-router';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-// import EditCurriculum from './components/editCurriculum/index';
+import EditCurriculum from './components/editCurriculum/index';
 import ProgramDisplay from './components/programDisplay/index';
+import CourseDetail from './components/courseDetail';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/* <div className='container' id="content">Content of page</div> */}
-      <ProgramDisplay />
-      {/* <EditCurriculum /> */}
-      <Footer />
-    </div>
+    <Router>
+      <div className="text-center">
+        <Header />
+        <Switch>
+          <Route path='/course/:courseId'><CourseDetail /></Route>
+          <Route path='/curriculum/:curriId'><EditCurriculum /></Route>
+          <Route exact path='/'><ProgramDisplay /></Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/courseDetail/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CourseDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h2 className='text-center'>This is course detail....<br/>Implement later..</h2>
+            </div>
+        )
+    }
+}
