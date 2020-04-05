9caa6467b5f6cd1cbdcfb72f507cbf5116a3a33a
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-04-05 20:15:48 +0700

    Basic Program Display Page

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import EditCurriculum from './components/editCurriculum/index';
-// import ProgramDisplay from './components/programDisplay/index';
+// import EditCurriculum from './components/editCurriculum/index';
+import ProgramDisplay from './components/programDisplay/index';
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <div className='container' id="content">Content of page</div> */}
-      {/* <ProgramDisplay /> */}
-      <EditCurriculum />
+      <ProgramDisplay />
+      {/* <EditCurriculum /> */}
       <Footer />
     </div>
   );
--- src/components/programDisplay/index.js
+++ src/components/programDisplay/index.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react'
 import state from '../../data/json/testData2'
-import Logo from '../../data/img/IULogo.png'
+import logo from '../../data/img/IU-logo.png'
+import SubMajor from './SubMajor';
 
 const programList = state.programTypes;
+const subMajorList = state.subMajors;
 
 export default class ProgramDisplay extends Component {
     render() {
         return (
             <div>
-                {programList.map(program => (<li>{program.name}</li>))}
-                <div className='container-fluid border' style={{height:'700px'}}>
-                    <div className='card border-dark m-1 p-1 h-50'>
-                        <div className='d-flex flex-row p-2'>
-                            <img className='school-img' scr={Logo} alt='logo'/>
-                            {/* {programList.map(program => (<h3>{program.name} Program</h3>))} */}
-                            <h4>International University Program</h4>
-                        </div>
-                        <div class="program-submajors d-flex flex-column flex-lg-row justify-content-center">
-                            <div class="card border-dark m-4 w-25 text-center">
-                                <h5 class="card-header border-dark">Computer Science</h5>
-                                <img class="card-img-top" src="logo192.png" alt="Logo"/>
-                                <div class="card-body">
-                                <p class="card-text">Computer Science <br/> for IU students</p>
-                                <footer class="blockquote-footer mb-2">Someone famous in <cite title="HCMIU">HCMIU</cite></footer>
-                                <a href="https://getbootstrap.com/docs/4.0/components/card/" class="btn btn-primary">To Program</a>
+                <div className='container-fluid border'>
+                    {programList.map(
+                        program => (
+                            <div className='card border-dark m-2 p-1'>
+                                <div className='d-flex flex-row p-2'>
+                                    <img src={logo} style={{maxWidth: "30px", maxHeight: "30px", marginRight:'5px'}} alt="logo"/>
+                                    <h4>{program.name} Program</h4>
                                 </div>
-                                <div class="card-footer border-dark text-muted">Edited 4 days ago</div> 
+                                <div class="d-flex flex-column flex-lg-row justify-content-left h-25">
+                                    {program.subMajorIds.map(
+                                        subMajorId => (
+                                            <SubMajor 
+                                                subMajor={subMajorList.find(function(subMajor) {
+                                                    return subMajor.id === subMajorId;
+                                                })}
+                                                program={program}
+                                            />
+                                        )
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        )
+                    )}
                 </div>
             </div>
         )
