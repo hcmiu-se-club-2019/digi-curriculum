fbc22046aefe51e803c083316d587013cb3745ce
dtahuy <dtahuy02@gmail.com>
2020-04-06 23:07:11 +0700

    Connect merged components to NavBar

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import EditCurriculum from './components/editCurriculum/index';
-import ProgramDisplay from './components/programDisplay/index';
-import CourseDetail from './components/courseDetail';
+import Home from "./components/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import EditCurriculum from "./components/editCurriculum/index";
+import ProgramDisplay from "./components/programDisplay/index";
+import CourseDetail from "./components/courseDetail";
 
-function App() {
-  return (
-    <Router>
-      <div className="text-center">
-        <Header />
-        <div className='text-left'>
-          <p>Example Links</p>
-          <ul>
-            <li>
-              <Link to="/">Program Display</Link>
-            </li>
-            <li>
-              <Link to="/curriculum/1/edit">Edit Curriculum</Link>
-            </li>
-            <li>
-              <Link to="/course/IT102IU">Web Application Course</Link>
-            </li>
-          </ul>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="text-center">
+          <Header />
+          {/* <div className="text-left">
+            <p>Example Links</p>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/program">Program Display</Link>
+              </li>
+              <li>
+                <Link to="/curriculum/1/edit">Edit Curriculum</Link>
+              </li>
+              <li>
+                <Link to="/course/IT102IU">Web Application Course</Link>
+              </li>
+            </ul>
+          </div> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/program">
+              <ProgramDisplay />
+            </Route>
+            <Route
+              exact
+              path="/course/:courseId"
+              component={CourseDetail}
+            ></Route>
+            <Route exact path="/curriculum/:curriId/edit">
+              <EditCurriculum />
+            </Route>
+          </Switch>
+          <Footer />
         </div>
-        <Switch>
-          <Route path='/course/:courseId' component={CourseDetail}></Route>
-          <Route path='/curriculum/:curriId/edit'><EditCurriculum /></Route>
-          <Route exact path='/'><ProgramDisplay /></Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
-    
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
